ad6d3b5f989b1809f0e31d125b54f9ba4b574cbc
Akashita08 <220879816+Akashita08@users.noreply.github.com>
2026-08-19 23:46:43 +0530

    Update helmet_transmitter.ino

--- code/helmet_transmitter/helmet_transmitter.ino
+++ code/helmet_transmitter/helmet_transmitter.ino
@@ -1,1 +1,28 @@
+#include <RH_ASK.h>
+#include <SPI.h>
 
+RH_ASK driver;  
+int ldr = A0;
+int led = 13;
+void setup() {
+  Serial.begin(9600);
+  driver.init();
+  pinMode(led, OUTPUT);
+  digitalWrite(13, HIGH);
+}
+
+void loop() {
+  int value = analogRead(ldr);
+  const char *msg;
+  if (value < 800) {   
+    msg = "1";
+    digitalWrite(led, HIGH);  
+  } else {
+    msg = "0";
+    digitalWrite(led, LOW);
+  }
+
+  driver.send((uint8_t *)msg, strlen(msg));
+  driver.waitPacketSent();
+  delay(200);
+}
